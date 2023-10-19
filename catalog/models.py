2ee7509be76270172c182228d8f04e521dfec8b3
Yulya Govorova <tishina1512@mail.ru>
2023-10-19 21:06:50 +1100

    added new migrate

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,3 @@
-
 import datetime
 from django.db import models
 
@@ -11,9 +10,7 @@ class Product(models.Model):
     product_preview = models.ImageField(upload_to='product/', verbose_name='изображение', **NULLABLE)
     product_category = models.CharField(max_length=100, verbose_name='категория')
     product_cost = models.FloatField(verbose_name='цена')
-    product_data_created = models.DateField(verbose_name='создан', default=datetime.date.today)
-    product_last_data_change = models.DateField(verbose_name='последнее изменение', default=datetime.date.today)
-    product_data_at_created = models.DateField(verbose_name='на удаление', default=datetime.date.today)
+    product_create_data = models.DateField(verbose_name='на удаление', default=datetime.date.today)
 
     def __str__(self):
         return f"{self.product_name}\nЦена: {self.product_cost}"
@@ -27,6 +24,7 @@ class Product(models.Model):
 class Category(models.Model):
     category_name = models.CharField(max_length=100, verbose_name='наименование')
     category_description = models.TextField(verbose_name='описание')
+    create_at = models.DateField(verbose_name='на удаление', default=datetime.date.today)
 
     def __str__(self):
         return f"{self.category_name} {self.category_description}"
