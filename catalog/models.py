e863f123a84ad532217724faafa9243cfd23ae76
Yulya Govorova <tishina1512@mail.ru>
2023-10-21 01:47:02 +1100

    added new shablon

--- catalog/models.py
+++ catalog/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=100, verbose_name='наименование')
     product_description = models.TextField(verbose_name='описание')
     product_preview = models.ImageField(upload_to='product/', verbose_name='изображение', **NULLABLE)
-    product_category = models.CharField(max_length=100, verbose_name='категория')
+    product_category = models.ForeignKey('Category', on_delete=models.CASCADE,  verbose_name='категория')
     product_cost = models.FloatField(verbose_name='цена')
     product_create_data = models.DateField(verbose_name='на удаление', default=datetime.date.today)
 
