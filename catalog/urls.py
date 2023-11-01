3f3cb9a9c7dd2868566bf447ddb8a73ef4833acf
Yulya Govorova <tishina1512@mail.ru>
2023-11-02 02:16:59 +1100

    added templatetags

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,14 @@
 from catalog.apps import CatalogConfig
 
-from catalog.views import index, contact
-
 from django.urls import path
+
+from catalog.views import index, contact, product
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', index, name='index'),
-    path('contact/', contact, name='contact')
+    path('contact/', contact, name='contact'),
+    path('product/<int:pk>', product)
 ]
 
