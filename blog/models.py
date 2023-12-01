ff79151370fd5ba4867b2617931df6f82cd45f82
Yulya Govorova <tishina1512@mail.ru>
2023-12-01 20:53:15 +1100

    added get_oobject

--- blog/models.py
+++ blog/models.py
@@ -7,11 +7,11 @@ NULLABLE = {'null': True, 'blank': True}
 class Blog(models.Model):
     title = models.CharField(max_length=100, verbose_name='заголовок')
     body = models.TextField(verbose_name='содержимое', **NULLABLE)
-    slug = models.CharField(max_length=100, verbose_name='slug')
+    slug = models.CharField(max_length=100, verbose_name='slug', **NULLABLE)
     preview = models.ImageField(upload_to='blog/', verbose_name='изображение', **NULLABLE)
     created_at = models.DateField(verbose_name='Создан', default=datetime.date.today)
     publication = models.BooleanField(verbose_name='опубликован', default=False)
-    views_count = models.BigIntegerField(verbose_name='количество просмотров', default=0)
+    views_count = models.IntegerField(verbose_name='количество просмотров', default=0)
 
     def __str__(self):
         return f'{self.title}\nСоздан: {self.created_at}'
