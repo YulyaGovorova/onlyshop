c79fe0214592235d84fe1d01a7b7bb68cba01f25
Yulya Govorova <tishina1512@mail.ru>
2023-12-30 03:17:45 +1100

    added new adres

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.views import LogoutView, LoginView, PasswordResetView
 from django.urls import path
-
 from django.views.generic import TemplateView
-from users.apps import UsersConfig
 
+from users.apps import UsersConfig
+from users.views import RegisterView, ProfileView, EmailVerify, PasswordRecoveryView
 
 app_name = UsersConfig.name
 
@@ -15,6 +15,12 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
-
+    path('confirm_email/', TemplateView.as_view(template_name='users/confirm_email.html'),
+         name='confirm_email'),
+    path('verify_email/<uidb64>/<token>/', EmailVerify.as_view(), name='verify_email'),
+    path('invalid_verify/', TemplateView.as_view(template_name='users/invalid_verify.html'),
+         name='invalid_verify'),
+    path('reset/<uidb64>/<token>/', PasswordResetView.as_view(), name='reset'),
+    path('recovery/', PasswordRecoveryView.as_view(template_name='users/restore_pass.html'), name='recovery'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,1 +1,82 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView as BaseLoginView, PasswordResetConfirmView, PasswordResetView
+from django.contrib.auth.views import LoginView as BaseLogoutView
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.tokens import default_token_generator as token_generator
+from config import settings
+from config.settings import EMAIL_HOST_USER
+from users.forms import UserForm, UserProfileForm, UserRecoveryForm
+from users.models import User
+from django.core.exceptions import ValidationError
+
+class LoginView(BaseLoginView):
+    template_name = 'users/login.html'
+
+
+class LogoutView(BaseLogoutView):
+    pass
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserForm
+    success_url = reverse_lazy('user:login')
+    template_name = 'users/register.html'
+
+    def form_valid(self, form):
+        new_user = form.save()
+        send_mail(
+            subject='Поздравляем с регистрацией',
+            message='',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[new_user.email]
+        )
+        return super().form_valid(form)
+
+class ProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+class EmailVerify(View):
+
+    def get(self, request, uidb64, token):
+        user = self.get_user(uidb64)
+
+        if user is not None and token_generator.check_token(user, token):
+            user.email_verify = True
+            user.save()
+            login(request, user)
+            return redirect('/')
+        return redirect('users:invalid_verify')
+
+    @staticmethod
+    def get_user(uidb64):
+        try:
+
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError,
+                User.DoesNotExist, ValidationError):
+            user = None
+        return user
+
+class PasswordRecoveryView(PasswordResetView):
+    template_name = 'users/restore_pass.html'
+    email_template_name = 'users/reset_email.html',
+    success_url = reverse_lazy('users:login')
+    from_email = EMAIL_HOST_USER
+    form_class = UserRecoveryForm
+
+
+
+class PasswordResetView(PasswordResetConfirmView):
+    post_reset_login = True
+    template_name = "users/password_reset_confirm.html"
+    success_url = reverse_lazy('users:login')
